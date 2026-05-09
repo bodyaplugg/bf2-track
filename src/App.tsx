583197f22ec11dd7720057bddce23174b60feddb
bodyaplugg <2007fix2007@gmail.com>
2026-05-09 13:52:25 +0300

    misc: optimized stylesheets

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import './globals.css';
 import Navbar from "./components/Navbar";
 import Home from './pages/Home'
 import Player from './pages/Player/Player'
+import SearchResults from './pages/SearchResults/SearchResults';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
     const handlePlayerSearch = (nickname: string, project: string) => {
-
+        // This function can be removed if not used elsewhere
     };
   return (
       <BrowserRouter>
@@ -16,6 +18,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/player/:pid" element={<Player />} />
+                <Route path="/search" element={<SearchResults />} />
             </Routes>
 
         </div>
